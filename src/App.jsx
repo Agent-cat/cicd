cbc34acf31c73ddee19a4c86e5fe72f415dbbec1
Agent-cat <2300031042@kluniversity.in>
2025-07-31 22:48:06 +0530

    removed all

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ const App = () => {
   return (
     <div className="bg-black w-full h-screen">
       
-        <div className="w-full h-screen bg-orange-400">1</div>
-        <div className="w-full h-screen bg-pink-400">2</div>
-        <div className="w-full h-screen bg-red-400">2</div>
+       
     </div>
   )
 }
