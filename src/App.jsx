8be0810c63b4c0a1a1bffdd0a7648650be13e048
Agent-cat <2300031042@kluniversity.in>
2025-07-31 22:47:37 +0530

    added a red div

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@
 const App = () => {
   return (
     <div className="bg-black w-full h-screen">
-      <h1 className="text-white">
-        Hello world
-        </h1>
+      
         <div className="w-full h-screen bg-orange-400">1</div>
         <div className="w-full h-screen bg-pink-400">2</div>
+        <div className="w-full h-screen bg-red-400">2</div>
     </div>
   )
 }
